250c8996da90c4a3a17abff276360305dd81d2cc
Prisyafandiafif Charifa <prisyafandiafif.charifa@gmail.com>
2016-09-14 05:18:47 -0400

    Selector script add

--- WarmUp/Assets/Scripts/GameManager.cs
+++ WarmUp/Assets/Scripts/GameManager.cs
@@ -3,20 +3,25 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour 
 {
-	public GameObject yellowDot;
-	public GameObject redDot;
-	public GameObject blueDot;
-	public GameObject greenDot;
+	// To store GameObjects of Dot
+	public GameObject[] dots;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		//init random dots on screen;
+		InitDots();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 	
+	}
+
+	// Init random dots
+	public void InitDots ()
+	{
+
 	}
 }

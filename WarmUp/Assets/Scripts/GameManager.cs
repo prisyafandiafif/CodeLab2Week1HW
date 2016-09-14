47fcdd06d706ad2b022d17979b7a113a5bdf76b7
Prisyafandiafif Charifa <prisyafandiafif.charifa@gmail.com>
2016-09-14 05:34:41 -0400

    Add instatiation function of dots

--- WarmUp/Assets/Scripts/GameManager.cs
+++ WarmUp/Assets/Scripts/GameManager.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour 
 {
-	// To store GameObjects of Dot
+	// To store gameobjects of Dot
 	public GameObject[] dots;
+
+	// To store a gameobject that contains the arrangement of each dot on the screen
+	public GameObject dotsContainer;
+
+	// To store the amount of row and column
+	public int rowAmount;
+	public int columnAmount;
 
 	// Use this for initialization
 	void Start () 
@@ -22,6 +29,20 @@ public class GameManager : MonoBehaviour
 	// Init random dots
 	public void InitDots ()
 	{
+		for (int i = 0; i < rowAmount; i++)
+		{
+			int randomIDForDots = Random.Range(0, 4);
 
+			GameObject newDot = Instantiate(dots[randomIDForDots]);
+
+			//make it a parent of dotsContainer
+			newDot.transform.parent = dotsContainer.transform;
+
+			//position it
+			//newDot.transform.position = new Vector3(, newDot.transform.position.y, newDot.transform.position.z);
+
+			//show it on screen
+			newDot.SetActive(true);
+		}
 	}
 }
